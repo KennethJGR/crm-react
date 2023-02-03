66800791503c012c20c52b5ede73fa5b8402dad9
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-02-03 14:35:04 -0600

    editing README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # crm-react
+Crm project with React Router DOM - Vite - TailwindCSS  
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import { useRoutes, Outlet } from "react-router-dom";
 const Layout = () => {
     return (
         <div className="md:flex md:min-h-screen">
-            <div className="md:w-1/4 bg-blue-600 px-5 py-10">
+            <div className="md:w-1/4 bg-blue-800 px-5 py-10">
                 
             </div>
         </div>
